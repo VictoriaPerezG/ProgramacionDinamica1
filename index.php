403719499724b9c21e23f684b14f903e97f59548
Patricio <36001508+patricio30@users.noreply.github.com>
2022-08-25 12:49:52 -0300

    subida de ejercicios1

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+
+  <title>Programacion Web Dinamica</title>
+    <link rel="stylesheet" type="text/css" href="vista/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="vista/css/estilos.css?v8">
+    <script type="text/javascript" src="vista/js/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="vista/js/bootstrap.min.js"></script>
+</head>
+
+<body class="fondo">
+  
+<div class="container" align="center">
+  
+  <div id="contenedorPrincipal"><br>
+    <span class="titulo0">PROGRAMACIÓN WEB DINÁMICA</span><br>
+    
+    <span class="titulo1">TP N°1</span><br>
+    <a class="linkEjercicio" href="vista/tp1ej1.php">Ejercicio 1</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej2.php">Ejercicio 2</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej3a.php">Ejercicio 3a</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej3b.php">Ejercicio 3b</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej4.php">Ejercicio 4</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej5.php">Ejercicio 5</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej6.php">Ejercicio 6</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej7.php">Ejercicio 7</a><br>
+    <a class="linkEjercicio" href="vista/tp1ej8.php">Ejercicio 8</a><br>
+    <br>
+
+    <span class="titulo1">TP N°2</span><br>
+    <a class="linkEjercicio" href="vista/tp2ej3.php">Ejercicio 3</a><br>
+    <a class="linkEjercicio" href="vista/tp2ej4.php">Ejercicio 4</a><br>
+    <br>
+
+    <span class="titulo1">TP N°3</span><br>
+    <a class="linkEjercicio" href="vista/tp3ej1.php">Ejercicio 1</a><br>
+    <a class="linkEjercicio" href="vista/tp3ej2.php">Ejercicio 2</a><br>
+    <a class="linkEjercicio" href="vista/tp2ej4.php">Ejercicio 3</a><br>
+    <br>
+
+  </div>
+</div>
+
+</body>
+</html>
